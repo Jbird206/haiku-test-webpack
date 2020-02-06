22a94daf15094629b5804aceddc85e63d9cd591f
Jason M <jbird206@gmail.com>
2020-02-05 16:24:17 -0800

    Add files via upload

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,4 +1,22 @@
-import { Haiku } from './../src/haiku.js';
+import { Haiku } from '../src/haiku.js';
 
 describe('Haiku', () => {
+  test('length of line1 is 5',() => {
+    var haiku = new Haiku("your moms box is big", "This is not 7 words", " ");
+    var haikuSplt = haiku.line1.split(" ")
+    expect(haikuSplt.length).toEqual(5);
+  })
+
+  test('length of line2 should be 7', () => {
+    var haiku = new Haiku("your moms box is big", "This is 7 words blah blah blah", " ");
+    var line2Words = haiku.line2.split(" ");
+
+    expect(line2Words.length).toEqual(7);
+  })
+
+  test('length should be 5', () => {
+    var haiku = new Haiku("dose it equal to 5?", "some more words here and here, yup", "and then like 5 here");
+    var line3Words = haiku.line3.split(" ");
+    expect(line3Words.length).toEqual(5);
+  })
 });
